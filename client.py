1693180942ecf3f4cee28ff6753f0560fce6fd96
DardanB <>
2022-11-06 19:50:32 +0100

    chat front end view

--- client.py
+++ client.py
@@ -32,3 +32,41 @@ def send(event=None):
 def on_closing(event=None):
     my_msg.set("{quit}")
     send()
+
+root = Tk()
+root.title("ChatIO")
+
+messages_frame = Frame(root)
+my_msg = StringVar()  # For the messages to be sent.
+my_msg.set("Shkruani mesazhet këtu.")
+scrollbar = Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=RIGHT, fill=Y)
+msg_list.pack(side=LEFT, fill=BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = Entry(root, textvariable=my_msg)
+entry_field.bind('<FocusIn>', on_entry_click)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = Button(root, text="Dërgo", command=send)
+send_button.pack()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+#Socket 
+HOST = 'localhost'
+PORT = 5000
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket.socket(AF_INET, SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+root.mainloop()

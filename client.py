5ee0b05e2319d73fb97866256b7eb90306d775af
DardanB <>
2022-11-06 18:02:25 +0100

    send and receive messages main function

--- client.py
+++ client.py
@@ -9,3 +9,20 @@ def on_entry_click(event):
     if firstclick:
         firstclick = False
         entry_field.delete(0, "end")
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(END, msg)
+        except OSError:
+            break
+
+
+def send(event=None):
+    msg = my_msg.get()
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        root.quit()

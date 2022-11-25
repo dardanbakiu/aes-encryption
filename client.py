c6bdcb0d6f76c7dca83edf6470a9827d8af1867c
DardanB <>
2022-11-26 00:26:46 +0100

    take key and store it

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 from AESencrypt import encrypt
 from AESdecrypt import decrypt
 
-key = '1234567890123456'
+key = ''
 
 def client_program():
     host = 'localhost'#socket.gethostname()  # as both code is running on same pc
@@ -16,11 +16,13 @@ def client_program():
     while message.lower().strip() != 'bye':
         client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
-        if 'DO_NOT_DECRYPT' not in data:
+        if 'DO_NOT_DECRYPT' not in data: #ktu eshte case masi te kryn pune me key
           decryptedDate = decrypt(key, data)
           print('Received from server: ' + decryptedDate)  # show in terminal
 
           message = input(" -> ")  # again take input
+        else:
+            key = data.split('DO_NOT_DECRYPT')[0]
 
     client_socket.close()  # close the connection
 
